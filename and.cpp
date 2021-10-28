1f3957a044593eb6940dd42b5d2f8f2fa3d811d4
UN-BEAT-ABLE <89098680+UN-BEAT-ABLE@users.noreply.github.com>
2021-10-28 19:50:38 +0530

    Create and.cpp

---
+++ and.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  
+   int n1, n2, n3;
+   int sum;
+
+   
+   cout << "Enter three numbers: ";
+   cin >> n1 >> n2 >> n3;
+
+   
+   sum = n1 + n2 + n3;
+   
+   
+   cout << "Sum = " << sum << endl; 
+
+   return 0;
+}
